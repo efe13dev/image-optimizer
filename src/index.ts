5c2bde05a4a43f44663f5fa2417c3337746f9df1
efe <eefee1987@gmail.com>
2024-10-29 07:39:44 +0100

    add first questions

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,40 @@
 import path from 'node:path';
 import { getMetadata, resizeImage } from './services/imageService';
 import { fileExists } from './utils/fileUtils';
+//import inquirer from 'inquirer';
+import { homedir } from 'node:os';
+import chalk from 'chalk';
+import fs from 'node:fs/promises';
 
 const MAX_IMAGE_HEIGHT = 2160;
 
 async function main(): Promise<void> {
-  const imageName = 'example.jpg';
-  const imagePath = path.join('public', imageName);
+  const desktopPath = path.join(homedir(), 'Desktop');
 
-  if (!(await fileExists(imagePath))) {
-    console.error(`La imagen ${imageName} no existe en la carpeta public.`);
+  const imagesPath = path.join(desktopPath, 'images');
+  console.log(chalk.green('Comprobando carpeta images en el escritorio...'));
+
+  if (!(await fileExists(imagesPath))) {
+    console.error(chalk.red('La carpeta images no existe en el escritorio 😥'));
     return;
   }
+
+  // Leer el contenido del directorio
+  const files = await fs.readdir(imagesPath);
+  const imageFiles = files.filter((file) =>
+    /\.(jpg|jpeg|png|gif|webp)$/i.test(file)
+  );
+
+  if (imageFiles.length === 0) {
+    console.error(
+      chalk.red(
+        'La carpeta images está vacía. Por favor, añade algunas imágenes 📸'
+      )
+    );
+    return;
+  }
+
+  console.log(chalk.green(`Se encontraron ${imageFiles.length} imágenes 🖼️`));
 
   const metadata = await getMetadata(imageName);
   if (metadata != null) {
